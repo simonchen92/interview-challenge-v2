ba1aa58d4011c0b4f0a2de8abc507f4dc92de231
simonchen92 <simon.k.chen92@gmail.com>
2020-01-30 20:08:22 -0500

    Delete some minor items which was not in use

--- src/components/list-card/ListCard.stories.js
+++ src/components/list-card/ListCard.stories.js
@@ -16,24 +16,16 @@ stories
   .add('with text', () => (
     <ListCard 
     header={'Header'} 
-    items={['a', 'b']} 
-    onClick={action('clicked')}>Hello ListCard</ListCard>
+    items={['Item 1', 'Item 2']}></ListCard>
   ))
   .add('with some emoji', () => (
     <ListCard
     header={'Header'} items={['😀 😎 👍 💯']}>
     </ListCard>
   ))
-  .add('with disabled prop activated', () => {
-    const disabled = boolean('Disabled', true);
-    return <ListCard 
-    header={'Header'}
-    items={['a', 'b']}
-    disabled={disabled} > Disabled </ListCard>
-  })
   .add('with custom themes', () => {
     const header = select('Custom Theme', headerList, headerList[0]);
     return <ListCard 
     header={header} 
-    items={['Item 1', 'Item 2', 'Item 3']}> Themed </ListCard>
+    items={['Item 1', 'Item 2', 'Item 3']}></ListCard>
    });
